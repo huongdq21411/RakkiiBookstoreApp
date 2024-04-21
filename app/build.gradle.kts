782297802b5d0c71fb36818d667d86c8c2dc517b
QuangNhut <nhutt21411@st.uel.edu.vn>
2024-04-21 10:47:36 +0700

    Quang Nhut

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,15 +35,13 @@ android {
 }
 
 dependencies {
-    implementation ("com.github.bumptech.glide:glide:4.14.2@aar")
+    implementation ("com.github.bumptech.glide:glide:4.14.2")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.firebase.database)
-    implementation(libs.glide)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-
 }
